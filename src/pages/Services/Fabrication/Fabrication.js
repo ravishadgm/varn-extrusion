060eff725b65ad389a493922607113e6c950c7c8
ravishadgm <ravishapatel.dgm@gmail.com>
2025-06-13 11:47:16 +0530

    changes in the button label

--- src/pages/Services/Fabrication/Fabrication.js
+++ src/pages/Services/Fabrication/Fabrication.js
@@ -1,8 +1,19 @@
+import CorpBlock from '@components/common/CorpBlock/CorpBlock'
 import React from 'react'
 
 function Fabrication() {
   return (
-    <div>Fabrication</div>
+      <CorpBlock
+      heading={
+        <>
+          All-In-One Manufacturing<br />
+          and Fabrication <br />
+      
+        </>
+      }
+      subheading="QB Corp. is one of the largest glulam manufacturers with CNC capabilities in the United States. Our facility offers comprehensive services, including the production of stock and custom glulam, CNC fabrication, attaching connections, and assembling."
+      buttonLabel="Contact An Expert"
+    />
   )
 }
 
