5f52f60df8bef54f4a5a7068eefd2dd69ca1db4a
ravishadgm <ravishapatel.dgm@gmail.com>
2025-06-13 17:22:16 +0530

    resolved router changes

--- src/pages/WoodPallets/Banner/Banner.js
+++ src/pages/WoodPallets/Banner/Banner.js
@@ -14,7 +14,7 @@ function Banner() {
       }
       subheading="QB Corporation is committed to increasing efficiency in our use of wood resources. Our LEMHI VALLEY WOOD PELLETS are manufactured using by-products generated during our processes, such as sawdust and end trims. The high-grade wood, primarily Douglas Fir, used to produce these pellets, is rated as ' Premium Grade ' by the pellet industry."
       buttonLabel="Get In Touch"
-         sideImage={Images.woodPellets_banner} 
+        sideImage={Images.woodPellets_banner} 
       greenBg={true} 
     />
   );
