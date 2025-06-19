43f015d9d4192146386712381cf7e90eaf6db47c
Harsh Vaddoriya <harshvaddoriya0319@gmail.com>
2025-06-19 15:19:46 +0530

    make responsive pages

--- src/pages/Products/Configurations/Curved/Curved.js
+++ src/pages/Products/Configurations/Curved/Curved.js
@@ -10,19 +10,27 @@ const Curved = () => {
                 <h1>Curved Beams and Arches</h1>
                 <p>Specify radius. A radius less than 600′ subjects to an extra charge as thinner laminates must be used.</p>
 
+            </div>
+
+
+
+
+            <div className={styles.imageBlock}>
+                <img src={CurvedImage} alt="Columns – Combination 2 or V8" />
+            </div>
+
+            <div className={styles.documentCard}>
+                <p>Additional information on arches and curves:</p>
                 <a
                     href="https://cdn.prod.website-files.com/655bc5fdd4556632911f083a/65dd02d3053855218dbb7e7d_ARCHES_AND_CURVES.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     <button className={styles.pdfButton}>
-                        View Document <FaRegFilePdf className={styles.pdfIcon} />
-                    </button>
-                </a>
-            </div>
 
-            <div className={styles.imageBlock}>
-                <img src={CurvedImage} alt="Columns – Combination 2 or V8" />
+                        View Document <FaRegFilePdf className={styles.pdfIcon} />
+
+                    </button></a>
             </div>
         </div>
     )
